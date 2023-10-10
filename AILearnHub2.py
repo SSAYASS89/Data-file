1497afbe3c996c0fdea4ad01582045c46e31bd0d
SSAYASS89 <109432522+SSAYASS89@users.noreply.github.com>
2023-10-10 11:49:52 +0100

    Add files via upload

---
+++ AILearnHub2.py
@@ -0,0 +1,54 @@
+https://github.com/AUTOMATIC1111/stable-diffusion-webui
+-y -c conda-forge opencv
+rich
+onnxruntime
+deforum_api_models
+numexpr
+https://github.com/Iyashinouta/sd-model-downloader
+https://github.com/Coyote-A/ultimate-upscale-for-automatic1111
+https://github.com/Mikubill/sd-webui-controlnet
+https://github.com/SSAYASS89/explorer-image
+https://github.com/IDEA-Research/DWPose
+https://github.com/fkunn1326/openpose-editor
+https://github.com/SSAYASS89/sd-openpose-editor-clone
+https://github.com/SSAYASS89/Downloader
+https://github.com/DominikDoom/a1111-sd-webui-tagcomplete
+https://github.com/SSAYASS89/tagger-v2
+https://github.com/fkunn1326/openpose-editor
+https://github.com/deforum-art/sd-webui-deforum
+https://github.com/Bing-su/adetailer
+pip install mediapipe
+pip install ultralytics
+https://github.com/hnmr293/sd-webui-cutoff
+https://github.com/Gourieff/sd-webui-reactor
+pip install insightface==0.7.3
+https://huggingface.co/datasets/AI-C/Data-File/resolve/main/inswapper_128.onnx
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_canny.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_canny_fp16.safetensors
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_inpaint.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_inpaint_fp16.safetensors
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_openpose.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_openpose_fp16.safetensors
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/models/ESRGAN
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/embeddings/embeddings.zip https://github.com/SSAYASS89/Data-file/raw/main/embeddings.zip
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/models/ESRGAN/upscalers.zip https://huggingface.co/datasets/AI-C/Data-File/resolve/main/upscalers.zip
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/embeddings
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/embeddings/embeddings.zip
+embeddings.zip
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/models/ESRGAN
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/models/ESRGAN/upscalers.zip
+upscalers.zip
+pip install pyngrok
+pip uninstall -y torch
+pip uninstall -y torchvision
+pip uninstall -y torchaudio
+pip install torch==2.0.1+cu117 torchvision==0.15.2+cu117 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu117
+--share --disable-safe-unpickle --theme=dark --no-half-vae --enable-insecure-extension --gradio-queue
+-q /home/studio-lab-user/sagemaker-studiolab-notebooks/
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/extensions-builtin/
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/models/
+insightface
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/models/insightface
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/models
+Lora
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/
+-rf webui.py
+"/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/webui.py" https://github.com/SSAYASS89/Data-file/raw/main/webui.py
+"/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui/config.json" https://github.com/SSAYASS89/Data-file/raw/main/config_kaggle.json
